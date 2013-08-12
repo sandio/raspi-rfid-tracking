1acd41e00bd21aa8f471b4566f77caadcb1a20c2
sandio <sandio.mama@gmail.com>
2013-08-12 13:40:09 +0100

    At discussion.

--- thesis/figures/R/error.grid.3d/tall.R
+++ thesis/figures/R/error.grid.3d/tall.R
@@ -5,7 +5,7 @@ pdf("tall.pdf", width=10, height=7)
 names = c("8cm","75cm", "170cm")
 
 s3d <- scatterplot3d(tall, xlim=c(0,4), ylim=c(0,6), zlim=c(0,2), lab=c(8, 6, 1), 
-	type="h", highlight.3d=T, angle=57, grid=TRUE, pch=19, 
+	type="h", highlight.3d=T, angle=57, grid=TRUE, pch=15, 
 	xlab="X (m)", ylab="Y (m)", zlab = "Average error (m)",
 	main="Accumulated errors for each position of the grid")
 
